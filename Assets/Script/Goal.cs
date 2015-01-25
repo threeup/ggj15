cc6cd78487bd4c9e3e76f845375cebc39812be0a
Sheridan Thirsk <threeup@gmail.com>
2015-01-25 13:25:29 -0800

    Goal prefab, moving platform

---
+++ Assets/Script/Goal.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : Item
+{
+    public AudioClip pickupSound;
+
+    public override void OnEnter(Collider2D other)
+    {
+        ActorEntity actor = other.GetComponent<ActorEntity>();
+        if( actor != null )
+        {
+            audio.PlayOneShot(pickupSound);
+            Director.Instance.ActorHitGoal(actor);
+        }
+    }
+
+}
